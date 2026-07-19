dbe3769a321012c83791be754e0e05d3996a56e3
erica-hayes <erica_hayes0917@hotmail.com>
2026-07-19 19:56:37 -0400

    Mini Project

--- my-first-react-app/src/App.tsx
+++ my-first-react-app/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { useReducer } from 'react'
+
+function reducer(state: number, action: { type: string }) {
+  switch (action.type) {
+    case 'INCREMENT_AGE':
+      if (state < 100) {
+        return state + 1
+      }
+      return state
+    default:
+      return state
+  }
+}
 
 export default function App() {
-  const user = 'Harsh' // Change this to '' to test the guest greeting
+  const [state, dispatch] = useReducer(reducer, 0)
 
-  if (user) {
-    return <h1 className="bgColor">Hello, {user}!</h1>
-  } else {
-    return <h1 className="bgColor">Hello Guest!</h1>
-  }
+  return (
+    <div className="bgColor">
+      <h1>User Age</h1>
+      <p>Current Age: {state}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT_AGE' })}>Increase Age</button>
+    </div>
+  )
 }
